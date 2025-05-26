c2008af5db939cdec127dbb42b480288746a6bc5
Quin <TheQuinbox@gmail.com>
2025-05-25 21:09:13 -0600

    Optimize text parser further.

--- src/text_parser.cpp
+++ src/text_parser.cpp
@@ -1,15 +1,17 @@
 #include "text_parser.hpp"
 #include <wx/filename.h>
-#include <wx/txtstrm.h>
 #include <wx/wfstream.h>
 
 std::unique_ptr<document> text_parser::load(const wxString& path) const {
 	wxFFileInputStream file_stream(path);
 	if (!file_stream.IsOk()) return nullptr;
-	wxBufferedInputStream bs(file_stream);
-	wxTextInputStream text_stream(bs);
+	constexpr size_t buffer_size = 1024 * 32;
+	wxCharBuffer buffer(buffer_size);
 	wxString content;
-	while (!file_stream.Eof()) content += text_stream.ReadLine() + "\n";
+	while (!file_stream.Eof()) {
+		size_t bytes_read = file_stream.Read(buffer.data(), buffer_size);
+		if (bytes_read > 0) content.append(wxString::FromUTF8(buffer.data(), bytes_read));
+	}
 	auto doc = std::make_unique<document>();
 	doc->set_title(wxFileName(path).GetName());
 	doc->set_author("Unknown");
