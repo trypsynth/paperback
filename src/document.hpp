b826461306b495f8b23d6bf27e80bc83ef68c568
Quin <TheQuinbox@gmail.com>
2025-05-28 12:42:14 -0600

    Move document interface into a single .hpp file for optimization and cleaner directories.

--- src/document.hpp
+++ src/document.hpp
@@ -4,12 +4,12 @@
 
 class document {
 public:
-	void set_title(const wxString& title);
-	void set_author(const wxString& author);
-	void set_text_content(const wxString& text_content);
-	const wxString& title() const;
-	const wxString& author() const;
-	const wxString& text_content() const;
+	void set_title(const wxString& title) {title_ = title;}
+	void set_author(const wxString& author) {author_ = author;}
+	void set_text_content(const wxString& text_content) {text_content_ = text_content;}
+	[[nodiscard]] const wxString& title() const {return title_;}
+	[[nodiscard]] const wxString& author() const {return author_;}
+	[[nodiscard]] const wxString& text_content() const {return text_content_;}
 
 private:
 	wxString title_;
