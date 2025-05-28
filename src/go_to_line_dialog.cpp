dda9866d9f20aa8dec6dfeb70dc6b88350795f82
Quin <TheQuinbox@gmail.com>
2025-05-28 08:27:07 -0600

    Select default value in go to line, label field properly.

--- src/go_to_line_dialog.cpp
+++ src/go_to_line_dialog.cpp
@@ -1,22 +1,26 @@
 #include "go_to_line_dialog.hpp"
 
 go_to_line_dialog::go_to_line_dialog(wxWindow* parent, wxTextCtrl* text_ctrl) :wxDialog{parent, wxID_ANY, "Go to line"}, textbox{text_ctrl} {
-	wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer* main_sizer = new wxBoxSizer(wxVERTICAL);
+	wxBoxSizer* line_sizer = new wxBoxSizer(wxHORIZONTAL);
+	auto* label = new wxStaticText(this, wxID_ANY, "Line number:");
 	spinner = new wxSpinCtrl(this, wxID_ANY);
+	line_sizer->Add(label, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
+	line_sizer->Add(spinner, 1, wxEXPAND);
 	spinner->SetRange(1, textbox->GetNumberOfLines());
 	long line;
 	textbox->PositionToXY(textbox->GetInsertionPoint(), 0, &line);
 	line++; // Account for PositionToXY being zero-based.
 	spinner->SetValue(line);
-	vbox->Add(new wxStaticText(this, wxID_ANY, "Line number:"), 0, wxALL, 5);
-	vbox->Add(spinner, 0, wxALL | wxEXPAND, 5);
-	wxBoxSizer* hbox = new wxBoxSizer(wxHORIZONTAL);
+	spinner->SetSelection(0, -1);
+	main_sizer->Add(line_sizer, 0, wxALL | wxEXPAND, 5);
+	wxBoxSizer* button_sizer = new wxBoxSizer(wxHORIZONTAL);
 	auto* ok_btn = new wxButton(this, wxID_OK);
 	ok_btn->SetDefault();
-	hbox->Add(ok_btn, 0, wxALL, 5);
-	hbox->Add(new wxButton(this, wxID_CANCEL), 0, wxALL, 5);
-	vbox->Add(hbox, 0, wxALIGN_CENTER);
-	SetSizerAndFit(vbox);
+	button_sizer->Add(ok_btn, 0, wxALL, 5);
+	button_sizer->Add(new wxButton(this, wxID_CANCEL), 0, wxALL, 5);
+	main_sizer->Add(button_sizer, 0, wxALIGN_CENTER);
+	SetSizerAndFit(main_sizer);
 }
 
 int go_to_line_dialog::line_number() const {
