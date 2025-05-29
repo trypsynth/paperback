05a0db38971e032ffc2a216885a0f9dcdffd2711
Quin <TheQuinbox@gmail.com>
2025-05-28 20:48:46 -0600

    Move command line parsing into a function.

--- src/app.cpp
+++ src/app.cpp
@@ -1,28 +1,31 @@
 #include "app.hpp"
-#include "main_window.hpp"
 #include "parser.hpp"
 #include <wx/filename.h>
 
 bool app::OnInit() {
-	main_window* frame = new main_window();
-	if (argc > 1) {
-		wxString path = wxString(argv[1]);
-		if (wxFileName::FileExists(path)) {
-			parser* par = find_parser_by_extension(wxFileName(path).GetExt());
-			if (!par)
-				wxMessageBox("No suitable parser found for " + path, "Error", wxICON_ERROR);
-			else {
-				std::unique_ptr<document> doc = par->load(path);
-				if (!doc)
-					wxMessageBox("Failed to load the document: " + path, "Error", wxICON_ERROR);
-				else
-					frame->open_document(path, std::move(doc));
-			}
-		} else
-			wxMessageBox("File not found: " + path, "Error", wxICON_ERROR);
-	}
+	frame = new main_window();
+	if (argc > 1) parse_command_line();
 	frame->Show(true);
 	return true;
+}
+
+void app::parse_command_line() {
+	wxString path = wxString(argv[1]);
+	if (!wxFileName::FileExists(path)) {
+		wxMessageBox("File not found: " + path, "Error", wxICON_ERROR);
+		return;
+	}
+	parser* par = find_parser_by_extension(wxFileName(path).GetExt());
+	if (!par) {
+		wxMessageBox("No suitable parser found for " + path, "Error", wxICON_ERROR);
+		return;
+	}
+	std::unique_ptr<document> doc = par->load(path);
+	if (!doc) {
+		wxMessageBox("Failed to load the document: " + path, "Error", wxICON_ERROR);
+		return;
+	}
+	frame->open_document(path, std::move(doc));
 }
 
 wxIMPLEMENT_APP(app);
--- src/app.hpp
+++ src/app.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include "main_window.hpp"
 #include <wx/wx.h>
 
 class app : public wxApp {
 public:
 	bool OnInit() override;
+
+private:
+	main_window* frame;
+	void parse_command_line();
 };
 
 wxDECLARE_APP(app);
