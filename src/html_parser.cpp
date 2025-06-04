d777e41dc6c6fe6a9a3e0c868798b9d81a26e09d
Quin <TheQuinbox@gmail.com>
2025-06-03 19:26:31 -0600

    Beginnings of HTML document support.

---
+++ src/html_parser.cpp
@@ -0,0 +1,25 @@
+#include "html_parser.hpp"
+#include "html_to_text.hpp"
+#include <Poco/FileStream.h>
+#include <Poco/SAX/InputSource.h>
+#include <Poco/SAX/SAXParser.h>
+#include <sstream>
+
+using namespace Poco;
+using namespace Poco::XML;
+
+std::unique_ptr<document> html_parser::load(const wxString& path) const {
+	FileInputStream stream(path.ToStdString());
+	if (!stream.good()) return nullptr;
+	InputSource src(stream);
+	SAXParser parser;
+	html_to_text handler;
+	parser.setContentHandler(&handler);
+	parser.parse(&src);
+	std::ostringstream oss;
+	for (const auto& line : handler.lines())
+		oss << line << "\n";
+	auto doc = std::make_unique<document>();
+	doc->set_text_content(oss.str());
+	return doc;
+}
---
+++ src/html_parser.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "parser.hpp"
+
+class html_parser : public parser {
+public:
+	wxString name() const override {return "HTML documents";}
+
+	const std::vector<wxString>& extensions() const override {
+		static const std::vector<wxString> exts = {"html", "htm"};
+		return exts;
+	}
+
+	parser_flags flags() const override { 
+		return parser_flags::supports_toc;
+	}
+
+	std::unique_ptr<document> load(const wxString& path) const override;
+};
+
+static html_parser html_par;
