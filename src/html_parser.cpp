be540d63ec245299c207ad9d26b0c0c6d3edc595
Quin <TheQuinbox@gmail.com>
2025-06-08 12:25:32 -0600

    Catch exceptions in html_parser.

--- src/html_parser.cpp
+++ src/html_parser.cpp
@@ -9,17 +9,23 @@ using namespace Poco;
 using namespace Poco::XML;
 
 std::unique_ptr<document> html_parser::load(const wxString& path) const {
-	FileInputStream stream(path.ToStdString());
-	if (!stream.good()) return nullptr;
-	InputSource src(stream);
-	SAXParser parser;
-	html_to_text handler;
-	parser.setContentHandler(&handler);
-	parser.parse(&src);
-	std::ostringstream oss;
-	for (const auto& line : handler.lines)
-		oss << line << "\n";
-	auto doc = std::make_unique<document>();
-	doc->text_content = oss.str();
-	return doc;
+	try {
+		FileInputStream stream(path.ToStdString());
+		if (!stream.good()) return nullptr;
+		InputSource src(stream);
+		SAXParser parser;
+		parser.setFeature(XMLReader::FEATURE_NAMESPACES, false);
+		parser.setFeature(XMLReader::FEATURE_NAMESPACE_PREFIXES, false);
+		html_to_text handler;
+		parser.setContentHandler(&handler);
+		parser.parse(&src);
+		std::ostringstream oss;
+		for (const auto& line : handler.lines)
+			oss << line << "\n";
+		auto doc = std::make_unique<document>();
+		doc->text_content = oss.str();
+		return doc;
+	} catch (const Poco::Exception& e) {
+		return nullptr;
+	}
 }
