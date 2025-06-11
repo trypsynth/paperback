8a3b16f322544956023576d44ebfb386910705a0
Quin <TheQuinbox@gmail.com>
2025-06-11 14:19:31 -0600

    Improve html parsing.

--- src/html_parser.cpp
+++ src/html_parser.cpp
@@ -11,8 +11,7 @@ std::unique_ptr<document> html_parser::load(const wxString& path) {
 	wxString content;
 	while (!file_stream.Eof()) content += text_stream.ReadLine();
 	html_to_text converter;
-	std::string html_content = content.ToStdString();
-	if (!converter.convert(html_content)) return nullptr;
+	if (!converter.convert(content.ToStdString())) return nullptr;
 	auto doc = std::make_unique<document>();
 	doc->text_content = converter.get_text();
 	return doc;
