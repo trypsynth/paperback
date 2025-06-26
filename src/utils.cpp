722957af7eea7c0471c947341f0751747a23b63a
Quin <TheQuinbox@gmail.com>
2025-06-25 22:31:10 -0600

    More find logic cleanups.

--- src/utils.cpp
+++ src/utils.cpp
@@ -3,21 +3,13 @@
 #include <string_view>
 #include "utils.hpp"
 
-long find_case_insensitive(const wxString& haystack, const wxString& needle, long start, bool forward) {
-	wxString haystack_lc = haystack.Lower();
-	wxString needle_lc = needle.Lower();
-	return forward ? haystack_lc.find(needle_lc, start) : haystack_lc.Left(start).rfind(needle_lc);
-}
-
 long find_text(const wxString& haystack, const wxString& needle, long start, bool forward, bool match_case) {
 	if (needle.empty()) return wxNOT_FOUND;
 	if (match_case)
-		if (forward)
-			return haystack.find(needle, start);
-		else
-			return haystack.Left(start).rfind(needle);
-	else
-		return find_case_insensitive(haystack, needle, start, forward);
+		return forward ? haystack.find(needle, start) : haystack.Left(start).rfind(needle);
+	wxString haystack_lc = haystack.Lower();
+	wxString needle_lc = needle.Lower();
+	return forward ? haystack_lc.find(needle_lc, start) : haystack_lc.Left(start).rfind(needle_lc);
 }
 
 std::string collapse_whitespace(std::string_view input) {
--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,5 @@
 #include <string>
 #include <wx/string.h>
 
-long find_case_insensitive(const wxString& haystack, const wxString& needle, long start, bool forward = true);
 long find_text(const wxString& haystack, const wxString& needle, long start, bool forward, bool match_case);
 std::string collapse_whitespace(std::string_view input);
