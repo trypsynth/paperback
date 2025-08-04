467dbf6edec17d43c8cc5d04a57f2b18ba96dbb9
Quin <trypsynth@gmail.com>
2025-08-04 17:01:41 -0600

    Update supported markdown formats.

--- src/markdown_parser.hpp
+++ src/markdown_parser.hpp
@@ -5,7 +5,7 @@ class markdown_parser : public parser {
 public:
 	wxString name() const override { return "Markdown Documents"; }
 	std::span<const wxString> extensions() const override {
-		static const wxString exts[] = {"md", "markdown", "mdown", "mkdown", "mkdwn"};
+		static const wxString exts[] = {"md", "markdown", "mdx", "mdown", "mdwn", "mkd", "mkdn", "mkdown", "ronn"};
 		return exts;
 	}
 	std::unique_ptr<document> load(const wxString& path) const override;
