171e569eafa2b06684a8d235cf0fb8fb004799eb
Quin <trypsynth@gmail.com>
2025-08-25 10:00:08 -0600

    Add license comments to source code.

--- src/text_parser.cpp
+++ src/text_parser.cpp
@@ -1,3 +1,13 @@
+/* text_parser.cpp - handles the reading of plain text files.
+ * This is decidedly the smallest and most simple parser, and it is recommended to use this file and its corresponding header as a base whenever adding a new parser.
+ *
+ * Paperback.
+ * Copyright (c) 2025 Quin Gillespie.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "text_parser.hpp"
 #include <wx/filename.h>
 #include <wx/txtstrm.h>
@@ -9,8 +19,7 @@ std::unique_ptr<document> text_parser::load(const wxString& path) const {
 	wxBufferedInputStream bs(file_stream);
 	wxTextInputStream text_stream(bs);
 	wxString content;
-	while (!bs.Eof())
-		content += text_stream.ReadLine() + "\n";
+	while (!bs.Eof()) content += text_stream.ReadLine() + "\n";
 	auto doc = std::make_unique<document>();
 	doc->title = wxFileName(path).GetName();
 	doc->author = "Unknown";
--- src/xml_to_text.cpp
+++ src/xml_to_text.cpp
@@ -1,3 +1,13 @@
+/* xml_to_text.cpp - handles the conversion of XML content into plaintext.
+ * This file has the same purpose as html_to_text.cpp, but it uses Poco's XML parser instead of lexbor so we can properly handle things like XHTML inside epub 2 books.
+ *
+ * Paperback.
+ * Copyright (c) 2025 Quin Gillespie.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "xml_to_text.hpp"
 #include "utils.hpp"
 #include <Poco/AutoPtr.h>
