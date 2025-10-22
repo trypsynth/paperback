6f23168c7aa26abc271b8e246050c38fc1885765
aryan <aryanchoudharyk2008@gmail.com>
2025-10-23 02:22:33 +0530

    properly set focus to the html table when pressing enter on a table

--- src/table_dialog.hpp
+++ src/table_dialog.hpp
@@ -4,9 +4,8 @@
  * Copyright (c) 2025 Quin Gillespie.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 #pragma once
 #include <wx/wx.h>
 #include <wx/webview.h>
@@ -14,13 +13,9 @@
 class table_dialog : public wxDialog {
 public:
 	table_dialog(wxWindow* parent, const wxString& title, const wxString& html);
-
 private:
 	wxWebView* web_view;
-	
-	// Handles the event fired when the page is fully loaded.
 	void on_webview_loaded(wxWebViewEvent& event);
-
-	// Handles messages sent from JavaScript back to C++.
 	void on_script_message(wxWebViewEvent& event);
+	void simulate_click();
 };
