3ccfeb8e1e84c37c5e05c07bff166561b02d341e
Quin <trypsynth@gmail.com>
2026-05-30 08:03:18 -0600

    use UIWindow subclass for magic tap via scene delegate
    UIWindow is guaranteed to be in the VoiceOver responder chain
    for all elements. Replace the NSPrincipalClass/UIApplication
    approach with a UIWindowSceneDelegate that installs MagicTapWindow
    as the key window, ensuring accessibilityPerformMagicTap() fires
    regardless of which element has VoiceOver focus.

--- ios/Paperback/PaperbackApp.swift
+++ ios/Paperback/PaperbackApp.swift
@@ -1,14 +1,44 @@
 import SwiftUI
+import UIKit
 
 extension Notification.Name {
 	static let pbMagicTap = Notification.Name("dev.paperback.magicTap")
 }
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		let config = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+		config.delegateClass = SceneDelegate.self
+		return config
+	}
+}
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
+		guard let windowScene = scene as? UIWindowScene else { return }
+		let window = MagicTapWindow(windowScene: windowScene)
+		window.rootViewController = UIHostingController(rootView: ContentView())
+		self.window = window
+		window.makeKeyAndVisible()
+	}
+}
+
+class MagicTapWindow: UIWindow {
+	override func accessibilityPerformMagicTap() -> Bool {
+		NotificationCenter.default.post(name: .pbMagicTap, object: nil)
+		return true
+	}
+}
+
 @main
 struct PaperbackApp: App {
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+			EmptyView()
 		}
 	}
 }
